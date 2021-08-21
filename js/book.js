7b81da7fadee821bf56c986fec8d0105cddd2075
unknown <mainulhasansadik@gmail.com>
2021-08-21 17:57:52 +0600

    Mac Book

---
+++ js/book.js
@@ -0,0 +1,75 @@
+function updateExtraCost(product,price){
+    const memoryCost =document.getElementById(product + '-cost');
+    memoryCost.innerText = price;
+    calculate();
+};
+
+function calculate(){
+    const productInput = document.getElementById('product-cost');
+    const productCost = parseFloat(productInput.innerText);
+
+    const memoryInput = document.getElementById('memory-cost');
+    const memoryCost = parseFloat(memoryInput.innerText);
+
+    const storageInput = document.getElementById('storage-cost');
+    const storageCost = parseFloat(storageInput.innerText);
+
+    const deliveryInput = document.getElementById('delivery-cost');
+    const deliveryCost = parseFloat(deliveryInput.innerText);
+    
+
+    //update total price
+    const totalPriceInput = document.getElementById('total-price');
+    const totalPrice = productCost + memoryCost + storageCost + deliveryCost;
+    totalPriceInput.innerText = totalPrice;
+
+    //update total
+    const totalInput = document.getElementById('total');
+    totalInput.innerText = totalPrice;
+    console.log(totalInput.innerText);
+    return totalPrice;
+}
+
+
+
+//Memeory cost Handler
+document.getElementById('memory-8').addEventListener('click',function(){
+    updateExtraCost('memory',0);
+})
+document.getElementById('memory-16').addEventListener('click',function(){
+    updateExtraCost('memory',180);
+})
+
+//Storage cost Handler
+
+document.getElementById('storage-25').addEventListener('click',function(){
+    updateExtraCost('storage',0);
+})
+document.getElementById('storage-512').addEventListener('click',function(){
+    updateExtraCost('storage',100);
+})
+document.getElementById('storage-1T').addEventListener('click',function(){
+    updateExtraCost('storage',180);
+})
+
+// Delivery cost Handler
+document.getElementById('delivery-option').addEventListener('click',function(){
+    updateExtraCost('delivery',0);
+})
+document.getElementById('delivery-option-2').addEventListener('click',function(){
+    updateExtraCost('delivery',20);
+})
+
+//pomo code
+document.getElementById('button-addon2').addEventListener('click',function(){
+    const discountInput = document.getElementById('pomo-code');
+    const discountCode = discountInput.value ;
+    const totalInput = document.getElementById('total');
+    if(discountCode == 'stevekaku'){
+        const total = calculate() - (calculate() / 5);
+        totalInput.innerText = total;
+    }
+    else{
+        totalInput.innerText = calculate();
+    }
+})
--- js/mac.js
+++ js/mac.js
@@ -1,28 +1,32 @@
-function updateMemoryCost(price){
-    const memoryCost =document.getElementById('memory-cost');
+function updateExtraCost(product,price){
+    const memoryCost =document.getElementById(product + '-cost');
     memoryCost.innerText = price;
 };
 
+//Memeory cost Handler
 document.getElementById('memory-8').addEventListener('click',function(){
-    updateMemoryCost(0);
+    updateExtraCost('memory',0);
 })
 document.getElementById('memory-16').addEventListener('click',function(){
-    updateMemoryCost(180);
+    updateExtraCost('memory',180);
 })
 
-
-//Extra storage cost handler
-function updateStorageCost(price){
-    const storageCost =document.getElementById('storage-cost');
-    storageCost.innerText = price;
-};
+//Storage cost Handler
 
 document.getElementById('storage-25').addEventListener('click',function(){
-    updateStorageCost(0);
+    updateExtraCost('storage',0);
 })
 document.getElementById('storage-512').addEventListener('click',function(){
-    updateStorageCost(100);
+    updateExtraCost('storage',100);
 })
 document.getElementById('storage-1T').addEventListener('click',function(){
-    updateStorageCost(180);
+    updateExtraCost('storage',180);
+})
+
+// Delivery cost Handler
+document.getElementById('delivery-option').addEventListener('click',function(){
+    updateExtraCost('delivery',0);
+})
+document.getElementById('delivery-option-2').addEventListener('click',function(){
+    updateExtraCost('delivery',20);
 })
